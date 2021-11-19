77d587507f891df7769c4e2bab918aeb6de89a63
Otavio Mortari <otaviom11@gmail.com>
2021-11-19 20:02:45 -0300

    Correções SQL

--- register.php
+++ register.php
@@ -1,5 +1,13 @@
+<link href="style.css" rel="stylesheet"/>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, user-scalable=no">
 <?php
 require "config.php";
+if (isset($_POST['usuario']) && empty($_POST['usuario']) or isset($_POST['email']) && empty($_POST['email']) or isset($_POST['senha']) && empty($_POST['senha'])) {
+    echo "<div class='container'><div class='mysqle'>Preencha todos os campos!</div></div> <br>
+    <div class='container'><a href='register.php'>Voltar</a></div>";
+    exit;
+}
 if(isset($_POST['usuario']) && !empty($_POST['usuario'])) {
     $usuario = $_POST['usuario'];
     $email = $_POST['email'];
@@ -24,15 +32,7 @@ if(isset($_POST['usuario']) && !empty($_POST['usuario'])) {
     $sql->execute();
     echo '<div class="container"><div class="mysql"><b>Cadastro efetuado com sucesso!</b>Usuário:'.$usuario.'<br>Email: '.$email.'</div></div>';}
 } 
-if (isset($_POST['usuario']) && empty($_POST['usuario']) or isset($_POST['email']) && empty($_POST['email']) or isset($_POST['senha']) && empty($_POST['senha'])) {
-    echo "<div class='container'><div class='mysqle'>Preencha todos os campos!</div></div> <br>
-    <div class='container'><a href='register.php'>Voltar</a></div>";
-    exit;
-}
 ?>
-<link href="style.css" rel="stylesheet"/>
-<meta charset="UTF-8"/>
-<meta name="viewport" content="width=device-width, user-scalable=no">
 <div class="container">
     <div class="registro"><h1>Registre-se</h1>
 <form method="POST">
